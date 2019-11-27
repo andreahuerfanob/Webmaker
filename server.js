bd4a0f673ca1fa90c0cda7ac442d03ff3f535040
andreahuerfanob <andreahuerfano@live.com>
2019-11-27 11:52:20 -0500

    server code

---
+++ server.js
@@ -0,0 +1,8 @@
+// import express library
+const express = require("express");
+// init express
+const app = express();
+// create server listening
+app.listen(3100, () => {
+  console.log("Server started on port 3100");
+});
