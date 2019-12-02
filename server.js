a904ed4f1df4915ea615bf74d1572898f500318e
andreahuerfanob <andreahuerfano@live.com>
2019-12-02 13:47:45 -0500

    server.js update

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-// import express library
 const express = require("express");
-// init express
+const path = require("path");
 const app = express();
-// create server listening
-app.listen(3100, () => {
-  console.log("Server started on port 3100");
+
+app.use(express.json());
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
